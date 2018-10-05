974d646b9f2bd806174b04a30f952531298fb1e2
Tanguy-bdjk <tanguybondjokwe@gmail.com>
2018-10-05 16:20:52 +0200

    js

---
+++ exercice_1/script.js
@@ -0,0 +1,4 @@
+// affichage de ma variable name dans un boite de dialogue j'appelle avec la fonction alert
+alert("YES!");
+// déclaration de la variable name initialisée avec une chaine de caractère "string"
+var name = "Tanguy";
---
+++ exercice_2/script.js
@@ -0,0 +1,7 @@
+// alert("Nom : Bond\nPrenom : tanguy\nVille : Paris");
+
+var firstName = "Bond";
+var lastName = "Tanguy";
+var city = "Paris";
+// je concatène mes chaines de caractères avec les variables
+alert("nom :" + firstName + "\n" + "prenom :" + lastName + "\n" + "ville :" + city);
